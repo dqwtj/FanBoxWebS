a7f3e674b445c2f15c8d99e54cd2dba242aa4d32
Tianju <wtshootj@Tianju-Wangs-MacBook-Air.local>
2014-09-17 16:53:39 +0800

    more card attr

--- db/migrate/002_create_cards.rb
+++ db/migrate/002_create_cards.rb
@@ -3,6 +3,14 @@ class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
       t.string :title
+      t.string :topic
+      t.string :event
+      t.string :source
+      t.string :size
+      t.string :image_type
+      t.string :image_height
+      t.string :image_width
+      t.string :c_type
       t.integer :height
       t.integer :favorites_count, default: 0
       t.string :base_url
